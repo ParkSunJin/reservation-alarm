6ced057a13f46e9078e5c10ff33f980b73c87f88
tjswlsdl135 <tjswlsdl135@naver.com>
2021-12-21 15:19:28 +0900

    add springBatchTest

---
+++ src/main/java/com/reservationalarm/springBatch/tasklets/TutorialTasklet.java
@@ -0,0 +1,18 @@
+package com.reservationalarm.springBatch.tasklets;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+@Slf4j
+public class TutorialTasklet implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(final StepContribution contribution, final ChunkContext chunkContext) throws Exception {
+        //log.debug("executed tasklet !!");
+        System.out.println("executed tasklet !!");
+        return RepeatStatus.FINISHED;
+    }
+}
