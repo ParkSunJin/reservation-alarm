b048c52a2a20800d7fc10494e15ad31bd8f056f7
tjswlsdl135 <tjswlsdl135@naver.com>
2021-12-22 15:13:19 +0900

    improve code

--- src/main/java/com/reservationalarm/movie/MovieBO.java
+++ src/main/java/com/reservationalarm/movie/MovieBO.java
@@ -14,70 +14,89 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class MovieBO {
     @Autowired
-    MovieMapper movieMapper;
+    MovieDAO movieMapper;
 
-    public Movie findMovieById(Integer movieId){
+    // 영화 정보 크롤링 url
+    private final static String crawlingURL = "http://www.cgv.co.kr/movies/?lt=1&ft=0";
+
+    public Movie findMovieById(Integer movieId) {
         return movieMapper.selectMovieById(movieId);
     }
 
-    public List<Movie> findMovieByTitle(String movieTitle){
-        if(movieTitle.isEmpty()) return null;
+    public List<Movie> findMovieByTitle(String movieTitle) {
+        if (movieTitle.isEmpty()) return null;
         return movieMapper.selectMovieByTitle(movieTitle);
     }
 
-    public List<Movie> findAllMovie(){
+    public List<Movie> findAllMovie() {
         return movieMapper.selectAllMovie();
     }
 
     public void insertMovieByCrawling() throws IOException, ParseException {
-        String crawlingURL = "http://www.cgv.co.kr/movies/?lt=1&ft=0";
-
-        Connection conn = Jsoup.connect(crawlingURL);
-        Document document = conn.get();
-
+        // 영화 정보 크롤링해서 DB에 저장하기 전에 그 전의 데이터는 모두 삭제한다.
         movieMapper.deleteAllMovie();
+
+        Document document = crawlingFromUrl();
 
         Elements moviesInfo = document.select("div.sect-movie-chart li");
 
-        for(Element movieInfo:moviesInfo){
-            // 영화 포스터 이미지 url
-            String movieImageSrc = movieInfo.select(".thumb-image img")
-                    .attr("abs:src");
-            // 영화 제목
-            String movieTitle = movieInfo.select(".title").text();
-            if(movieImageSrc == "") continue;
-            // 예매율
-            String movieScoreStr = movieInfo.select(".score strong")
-                    .text()
-                    .replace("예매율","")
-                    .replace("%","");
-            Double movieScore = Double.parseDouble(movieScoreStr);
-            // 개봉 날짜
-            String openingDateStr = movieInfo.select(".txt-info").text()
-                    .substring(0, 10);
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.mm.dd");
-            Date openingDate = dateFormat.parse(openingDateStr);
+        moviesInfo.stream()
+                .map(this::convert)
+                .filter(Objects::nonNull)
+                .forEach(movieMapper::insertMovie);
+    }
 
-            // 개봉 여부
-            Elements dday = movieInfo.select(".dday");
-            Boolean isOpened = (dday.isEmpty()) ? true : false;
+    private Document crawlingFromUrl() throws IOException {
+        Connection conn = Jsoup.connect(crawlingURL);
+        Document document = conn.get();
+        return document;
+    }
 
-            // 예매 링크
-            String reservationLink = movieInfo.select(".like a")
-                    .attr("abs:href");
-
-            movieMapper.insertMovie(Movie.builder()
-                    .movieTitle(movieTitle)
-                    .movieImageSrc(movieImageSrc)
-                    .movieScore(movieScore)
-                    .openingDate(openingDate)
-                    .isOpened(isOpened)
-                    .reservationLink(reservationLink)
-                    .build());
+    private Movie convert(Element movieInfo) {
+        // 영화 포스터 이미지 url
+        String movieImageSrc = movieInfo.select(".thumb-image img")
+                .attr("abs:src");
+        // 영화 제목
+        String movieTitle = movieInfo.select(".title").text();
+        if (movieImageSrc == "") return null;
+        // 예매율
+        String movieScoreStr = movieInfo.select(".score strong")
+                .text()
+                .replace("예매율", "")
+                .replace("%", "");
+        Double movieScore = Double.parseDouble(movieScoreStr);
+        // 개봉 날짜
+        String openingDateStr = movieInfo.select(".txt-info").text()
+                .substring(0, 10);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
+        Date openingDate = null;
+        try {
+            openingDate = dateFormat.parse(openingDateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
         }
+
+        // 개봉 여부
+        Elements dday = movieInfo.select(".dday");
+        Boolean isOpened = (dday.isEmpty()) ? true : false;
+
+        // 예매 링크
+        String reservationLink = movieInfo.select(".like a")
+                .attr("abs:href");
+
+        return Movie.builder()
+                .movieTitle(movieTitle)
+                .movieImageSrc(movieImageSrc)
+                .movieScore(movieScore)
+                .openingDate(openingDate)
+                .isOpened(isOpened)
+                .reservationLink(reservationLink)
+                .build();
     }
 }
--- src/main/java/com/reservationalarm/movie/MovieDAO.java
+++ src/main/java/com/reservationalarm/movie/MovieDAO.java
@@ -1,15 +1,14 @@
 package com.reservationalarm.movie;
 
 import com.reservationalarm.movie.domain.Movie;
-import com.reservationalarm.user.domain.User;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
-public interface MovieMapper {
+public interface MovieDAO {
     Movie selectMovieById(Integer movieId);
-    List<Movie>` selectMovieByTitle(String movieTitle);
+    List<Movie> selectMovieByTitle(String movieTitle);
     List<Movie> selectAllMovie();
     void insertMovie(Movie movie);
     void updateMovie(Movie movie);
