04682181d24a1e8f1415e2fdcd420c5adc176fbe
tjswlsdl135 <tjswlsdl135@naver.com>
2022-02-09 11:55:25 +0900

    feat : add HallType as enum

--- src/main/java/com/reservationalarm/hallType/domain/HallType.java
+++ src/main/java/com/reservationalarm/hallType/domain/HallType.java
@@ -1,4 +1,17 @@
 package com.reservationalarm.hallType.domain;
 
-public class HallType {
+/*
+* TODO : enum 공부!
+* */
+public enum HallType {
+    _4DX("4DX"), _2D("2D"), IMAX("IMAX");
+
+    private final String value;
+    HallType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
 }
