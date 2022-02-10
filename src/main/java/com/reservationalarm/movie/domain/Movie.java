7835cdb617b86578691f5d7ae23ce5160e7793a8
tjswlsdl135 <tjswlsdl135@naver.com>
2022-02-10 13:15:07 +0900

    refactor : AllArgsConstructor, NoArgsConstructor annotation 추가

--- src/main/java/com/reservationalarm/movie/domain/Movie.java
+++ src/main/java/com/reservationalarm/movie/domain/Movie.java
@@ -1,14 +1,14 @@
 package com.reservationalarm.movie.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Date;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
     public Integer movieId;
     public String movieTitle;
